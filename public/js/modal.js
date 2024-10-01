30fc65e0b3250e2f50b46be720beed74e05dfa17
Ibnu Alqalibi <56583058+ibnAlqalibi@users.noreply.github.com>
2024-10-02 02:32:57 +0700

    slicing comment
    - comment modal
    - reply modal
    - favIcon

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,25 +1,51 @@
-// Mendapatkan elemen modal
-var modal = document.getElementById("myModal");
+const modal = document.querySelector("#commentModal");
+const replymodal = document.querySelector("#replyModal");
+const btn = document.querySelector("#openModalBtn");
+const span = document.querySelector(".close");
+const replyspan = document.querySelector(".replyclose");
+const writeReply = document.querySelector("#writeReply");
+const cancelReply = document.querySelector("#cancelReply");
+// const commentCard = document.querySelector(".card");
 
-// Mendapatkan tombol yang membuka modal
-var btn = document.getElementById("openModalBtn");
+//buka modal
+btn.onclick = () => {
+    modal.style.display = "flex";
+};
 
-// Mendapatkan elemen <span> yang menutup modal
-var span = document.getElementsByClassName("close")[0];
-
-// Saat tombol diklik, buka modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// Saat tombol close (x) diklik, tutup modal
-span.onclick = function() {
+// tombol tutup modal
+span.onclick = () => {
     modal.style.display = "none";
-}
+};
 
-// Saat pengguna mengklik di luar modal, tutup modal
-window.onclick = function(event) {
+// tombol tutup modal
+replyspan.onclick = () => {
+    replymodal.style.display = "none";
+};
+
+//tutup modal lewat luar
+window.onclick = (event) => {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-}
+    if (event.target == replymodal) {
+        replymodal.style.display = "none";
+    }
+};
+
+//handle click card
+const commentCard = (element) => {
+    // modal.style.display = "flex";
+    const elementId = element.id;
+    replymodal.style.display = "flex";
+    console.log("ID dari elemen yang diklik: " + elementId);
+};
+
+//handle reply
+writeReply.onclick = () => {
+    document.querySelector(".replying").style.display = "inherit";
+    document.querySelector("#replyActions").style.display = "none";
+};
+cancelReply.onclick = () => {
+    document.querySelector(".replying").style.display = "none";
+    document.querySelector("#replyActions").style.display = "inherit";
+};
