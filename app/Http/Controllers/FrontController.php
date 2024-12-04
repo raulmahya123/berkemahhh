10c30c88377296a54345fe0f69aa312eab561852
AnasMaruf <anasmaruf1101@gmail.com>
2024-12-04 12:50:35 +0700

    Update

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -104,8 +104,19 @@ class FrontController extends Controller
 
   public function pricing()
   {
-     $allPaket = Paket::all();
-      return view('front.pricing',compact('allPaket'));
+      if (Auth::check()) {
+          if (Auth::user()->hasActiveSubscription()) {
+              return redirect()->route('front.index');
+          }
+      } else {
+          return redirect()->route('login');
+      }
+      $user = Auth::user();
+      $allPaket = Paket::with('keypointPakets')->get();
+      $transaction = $user->transactions()->where('status', 'success')->first(); // Ambil transaksi yang tidak sukses
+
+
+      return view('front.pricing',compact('allPaket','transaction'));
   }
 
 }
